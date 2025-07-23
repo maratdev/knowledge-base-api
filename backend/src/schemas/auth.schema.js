e7af40bbf9bbdbc13297f93e6f2e743c38b1c25f
m.zakaryev <m.zakaryev@umnico.com>
2025-07-23 14:14:04 +0300

    chore: remove unintended .js files from repository

--- backend/src/schemas/auth.schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthSchema = void 0;
-const zod_1 = require("zod");
-exports.AuthSchema = zod_1.z.object({
-    email: zod_1.z.string().nonempty('Email is required').email(),
-    password: zod_1.z.string().min(6, 'Password must be at least 6 characters')
-});
